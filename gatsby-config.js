97ea12f86e45d3da21575954b8c96f25010e434a
AstroGoat <astrogoat@localhost.localdomain>
2020-09-19 12:52:44 +0200

    Initial Commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,6 +50,7 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-netlify-cms`,
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
